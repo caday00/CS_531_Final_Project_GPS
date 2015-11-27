da1db3b0d1beb355d351e9d9f6ac2a12545eb98f
Azrael978 <Azrael978@users.noreply.github.com>
2015-11-27 00:02:02 -0500

    Added hyperlink to Google Maps
    Needs work to convert lat/long to actual decimal notation for proper display in Google Maps.

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /*
  CS531 FINAL PROJECT - (WORK IN PROGRESS)
  
- SAMPLE DATA FROM ARDUINO TEST OUTPUT 
+ SAMPLE DATA FROM ARDUINO TEST OUTPUT
  
  $GPRMC,232155.000,A,3854.7070,N,07728.5584,W,22.81,28.84,181115,,,A*70
  
@@ -39,7 +39,7 @@
  11   = E or W
  12   = Checksum
  ---------------------------------------------------------------------------------------
-*/
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -49,32 +49,37 @@
 #define MAXREAD 10000
 
 // GPS DATA TYPE 1 CONTAINING LAT & LONG, DATE, TIME, AND OTHER PROPERTIES
-struct gprmc {    
+struct gprmc {
     const char *gps_reading_type;
     float fix_time;
     char status;
-    float latitude;
-    char lat_direction;
-    float longitude;
-    char long_direction;
+    float latitude;        // <- This needs to get converted to decimal prior to use in Google Maps. Need function.
+    char lat_direction;    //
+    float longitude;       // <- This needs to get converted to decimal prior to use in Google Maps. Need function.
+    char long_direction;   //
     float speed;
     float track_angle;
     int date;
     float mag_var;
     char mag_var_direction;
-    char *checksum;    
+    char *checksum;
 };
 
 // ARRAY OF STRUCTURE POINTERS TO HOLD GPS DATA LOADED INTO STRUCTURES
 struct gprmc *gpsDataType1[MAXREAD];
 
-// FUNCTION PROTOTYPES TO PARSE & LOAD LINE DATA OF THE TWO GPS DATA TYPES
+// FUNCTION PROTOTYPE TO PARSE & LOAD LINE DATA OF THE GPS DATA
 struct gprmc *loadGPRMCData(char *line);
 struct tm  * convertDateTime(float utc, int date);
+
+// FUNCTION PROTOTYPE TO PRINT THE GPS DATA TO FILE AND FORMAT FOR HTML DISPLAY
 void *print_html(void *arg);
 
-int main(int argc, char **argv) {        
-    // LOAD ARDUINO GPS DATA FILE FROM COMMAND LINE ARGUMENT    
+// FREE STRUCT POINTERS FROM MEMORY
+void clean();
+
+int main(int argc, char **argv) {
+    // LOAD ARDUINO GPS DATA FILE FROM COMMAND LINE ARGUMENT
     char *file_name = argv[1];
     
     FILE *fp;
@@ -84,49 +89,50 @@ int main(int argc, char **argv) {
     {
         perror("Error while opening the file.\n");
         exit(EXIT_FAILURE);
-    }    
-	
-    // CREATE PRINTING THREAD    
-    pthread_t print_html_thread;
-
-    if(pthread_create(&print_html_thread, NULL, print_html,NULL)) {        
-        perror("Error creating thread\n");
-        exit(EXIT_FAILURE);        
     }
-	
-    /*     
-     READ LINE DATA IN AND DETERMINE WHICH TYPE OF GPS DATA IT IS. INSPECT THE READ LINE AND READ THE FIRST SEVEN
-     CHARS TO DETERMINE WHICH STRUCTURE TO LOAD. THE DATA ALTERNATES BETWEEN $GPRMC AND$GPGGA DATA TYPES
-     */    
+    
+    // CREATE PRINTING THREAD
+    pthread_t print_html_thread;
+    
+    if(pthread_create(&print_html_thread, NULL, print_html,NULL)) {
+        perror("Error creating thread\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    /*
+     READ LINE DATA IN AND DETERMINE WHICH TYPE OF GPS DATA IT IS BY INSPECTING THE FIRST SEVEN CHARS.
+     */
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
     int line_num = 0;
     char peek[8];
     
-    while ((read = getline(&line, &len, fp)) != -1) {       
-        strncpy(peek, line, 7);        
-        if(strcmp(peek, "$GPRMC," ) == 0)          
-            gpsDataType1[line_num++] =  loadGPRMCData(line);		
-        else {            
+    while ((read = getline(&line, &len, fp)) != -1) {
+        strncpy(peek, line, 7);
+        if(strcmp(peek, "$GPRMC," ) == 0)
+            gpsDataType1[line_num++] =  loadGPRMCData(line);
+        else {
             perror("Error. GPS data specification not found.");
             exit(EXIT_FAILURE);
         }
-    } 
+    }
     pthread_join(print_html_thread, NULL);
-    fclose(fp);    
-    pthread_exit(NULL);    
-    return 0;    
+    fclose(fp);
+    pthread_exit(NULL);
+
+    clean();
+    return 0;
 }
 
 /* PARSER FOR $GPRMC LINE OF DATA. LOADS THE PARSED COMMA SEPARATED TOKENS INTO THE DESIGNATED STRUCTURE  */
-struct gprmc *loadGPRMCData(char *line) {    
-    struct gprmc *p = malloc(sizeof(struct gprmc)); 
+struct gprmc *loadGPRMCData(char *line) {
+    struct gprmc *p = malloc(sizeof(struct gprmc));
     char *token;
     int token_num = 0;
     
     while ((token = strsep(&line, ",")) != NULL) {
-        switch(token_num) {                
+        switch(token_num) {
             case 0:
                 p->gps_reading_type = token;
                 token_num++;
@@ -196,29 +202,37 @@ void *print_html(void *arg){
         perror("Error creating the file.\n");
         exit(EXIT_FAILURE);
     }
-    fprintf(fp, "<body>\n");
-	fprintf(fp, "<meta charset=\"UTF-8\">");
-	fprintf(fp, "\t<table style=\"width:100%\">");
-	fprintf(fp, "\t<tr>");
-	fprintf(fp, "\t<th>Row</th>\n\t<th>Fix Time</th>\n\t<th>Status</th>\n\t<th>Latitude</th>\n\t<th>Lat Direction</th>\n\t<th>Longtitude</th>\n\t<th>Long Direction</th>\n\t<th>Date/Time</th>");
-	fprintf(fp, "</tr>");
     
-  	int i = 0;
-	for (i; gpsDataType1[i] != '\0'; i++) {
-            struct tm * date_time = convertDateTime(gpsDataType1[i]->fix_time,gpsDataType1[i]->date);
-            fprintf(fp, "\t<tr><td><center>%d</center></td><td><center>%.4f</center></td><td><center>%c</center></td><td><center>%.4f</center></td><td><center>%c</center></td><td><center>%.4f</center></td><td><center>%c</center></td><td><center>%d/%d/%d %d:%d.%d</center></td></tr>",
-                                   i,
-                                   gpsDataType1[i]->fix_time,
-								   gpsDataType1[i]->status,
-                                   gpsDataType1[i]->latitude, 
-                                   gpsDataType1[i]->lat_direction,//N or S
-                                   gpsDataType1[i]->longitude, 
-                                   gpsDataType1[i]->long_direction, //E or W
-                                   date_time->tm_mon, date_time->tm_mday, date_time->tm_year,date_time->tm_hour, date_time->tm_min, date_time->tm_sec);
-                free(date_time);
-        }	
-	fprintf(fp, "\n\t</table>");
-	fprintf(fp, "\n</body>");    
+    fprintf(fp, "<!DOCTYPE html>\n");
+    fprintf(fp, "<html>\n");
+    fprintf(fp, "<body>\n");
+    fprintf(fp, "<meta charset=\"UTF-8\">");
+    fprintf(fp, "\t<table style=\"width:100%\">");
+    fprintf(fp, "\t<tr>");
+    fprintf(fp, "\t<th>Row</th>\n\t<th>Fix Time</th>\n\t<th>Status</th>\n\t<th>Latitude</th>\n\t<th>Lat Direction</th>\n\t<th>Longtitude</th>\n\t<th>Long Direction</th>\n\t<th>Date/Time</th>\n\t<th>Link to Map</th>");
+    fprintf(fp, "</tr>");
+    
+    int i = 0;
+    for (i; gpsDataType1[i] != '\0'; i++) {
+        struct tm * date_time = convertDateTime(gpsDataType1[i]->fix_time,gpsDataType1[i]->date);
+        fprintf(fp, "\t<tr><td><center>%d</center></td><td><center>%.4f</center></td><td><center>%c</center></td><td><center>%.4f</center></td><td><center>%c</center></td><td><center>%.4f</center></td><td><center>%c</center></td><td><center>%d/%d/%d %d:%d:%d</center></td><td><center><a href=\"https://www.google.com/maps/?q=%.4f,%.4f\" target=\"_blank\">Click</a></center></td></tr>",
+                
+                
+                i,
+                gpsDataType1[i]->fix_time,
+                gpsDataType1[i]->status,
+                gpsDataType1[i]->latitude,
+                gpsDataType1[i]->lat_direction,//N or S
+                gpsDataType1[i]->longitude,
+                gpsDataType1[i]->long_direction, //E or W
+                date_time->tm_mon, date_time->tm_mday, date_time->tm_year,date_time->tm_hour, date_time->tm_min, date_time->tm_sec, gpsDataType1[i]->latitude,gpsDataType1[i]->longitude);
+        
+        free(date_time);
+    }
+    fprintf(fp, "\n\t</table>");
+    fprintf(fp, "\n</body>");
+    fprintf(fp, "\n</html>");
+    fclose(fp);
     return NULL;
 }
 
@@ -227,13 +241,22 @@ struct tm  * convertDateTime(float utc, int date){
     
     struct tm *p = malloc(sizeof(struct tm));
     int time = (int) utc;
-	
+    
     p->tm_hour = (time / 10000) % 100;
     p->tm_min = (time / 100) % 100;
     p->tm_sec = time % 100;
     p->tm_mday = (date / 10000) % 100;
     p->tm_mon = (date / 100) % 100;
     p->tm_year = (date % 100) + 2000;
-	
+    
     return p;
+}
+
+
+/* FREE-UP MEMORY  */
+void clean(){
+    
+    for (int i = 0; gpsDataType1[i] != '\0'; i++)
+        free(gpsDataType1[i]);
+    
 }
